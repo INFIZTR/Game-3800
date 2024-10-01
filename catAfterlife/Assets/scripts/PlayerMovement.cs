03fe2e3dbb5acd235aed3c660c29c0bbec5b6da2
yutong2000 <2741201152@qq.com>
2024-10-01 15:32:02 -0400

    graybox implement

---
+++ catAfterlife/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get input from the player (WASD or arrow keys)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Move the player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
