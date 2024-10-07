67672cd749ed0f46be417eb43caa1a934d77912b
infiztr <zhang.tin@northeastern.edu>
2024-10-06 21:47:06 -0400

    Arknight style talking GUI

---
+++ catAfterlife/Assets/scripts/TextManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public int dialogIndex = 0;
+    private string[] dialogRows;
+    public TextAsset textAsset;
+    public SpriteRenderer spriteL;
+    public SpriteRenderer spriteR;
+
+    public TMP_Text nameText;
+    public TMP_Text dialogText;
+    
+    public List<Sprite> sprites = new List<Sprite>();
+    Dictionary<string, Sprite> nameImageDict = new Dictionary<string, Sprite>();
+
+    [FormerlySerializedAs("istext")] public bool isText;
+    
+    public GameObject dialogButton;
+    public Transform dialogButtonGroup;
+    
+    public GameObject TalkingGUI;
+
+    public void Awake()
+    {
+        nameImageDict["A"] = sprites[0];
+        nameImageDict["B"] = sprites[1];
+    }
+
+    private void UpdateText(string _name, string _dialog)
+    {
+        nameText.text = _name;
+        dialogText.text = _dialog;
+    }
+
+    private void UpdateImage(string _name, string position)
+    {
+        if (position == "L")
+        {
+            spriteL.sprite = nameImageDict[_name];
+        }
+        else if (position == "R")
+        {
+            spriteR.sprite = nameImageDict[_name];
+        }
+    }
+    
+    private void GenerateText(TextAsset dialog)
+    {
+        dialogRows = dialog.text.Split('\n');
+        PrintText();
+    }
+
+    private void PrintText()
+    {
+        for (int i = 0; i < dialogRows.Length; i++)
+        {
+            string[] cells = dialogRows[i].Split(',');
+            if (cells[0] == "#" && int.Parse(cells[1]) == dialogIndex)
+            {
+                isText = true;
+                UpdateText(cells[2], cells[4]);
+                UpdateImage(cells[2], cells[3]);
+
+                dialogIndex = int.Parse(cells[5]);
+                break;
+            }
+            else if (cells[0] == "&" && int.Parse(cells[1]) == dialogIndex)
+            {
+                isText = false;
+                GenerateBotton(i);
+            }
+            else if (cells[0] == "END" && int.Parse(cells[1]) == dialogIndex)
+            {
+                dialogIndex = 0;
+                Start();
+                TalkingGUI.SetActive(false);
+            }
+        }
+    }
+
+    private void GenerateBotton(int index)
+    {
+        string[] cells = dialogRows[index].Split(',');
+        if (cells[0] == "&")
+        {
+            GameObject button = Instantiate(dialogButton,dialogButtonGroup);
+            button.GetComponentInChildren<TMP_Text>().text = cells[4];
+            button.GetComponent<Button>().onClick.AddListener(delegate { OnButtonClick(int.Parse(cells[5]));});
+            GenerateBotton(index+1);
+        }
+    }
+
+    private void OnButtonClick(int id)
+    {
+        dialogIndex = id;
+        PrintText();
+        for (int i = 0; i < dialogButtonGroup.childCount; i++)
+        {
+            Destroy(dialogButtonGroup.GetChild(i).gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateText(textAsset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isText)
+        {
+            GenerateText(textAsset);
+        }
+    }
+}
