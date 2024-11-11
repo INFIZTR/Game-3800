8b3cd5e7ba6046048dbe5aefab817f378bacbf67
infiztr <zhang.tin@northeastern.edu>
2024-11-11 18:10:14 -0500

    lots of things

---
+++ catAfterlife/Assets/scripts/Tutorial.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject CanvesGUI;
+    bool firstTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstTime = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (firstTime)
+        {
+            firstTime = false;
+            CanvesGUI.SetActive(true);
+        }
+        else
+        {
+            if (Input.anyKey)
+            {
+                CanvesGUI.SetActive(false);
+            }
+        }
+    }
+}
