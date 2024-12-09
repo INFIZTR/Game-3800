5a2d1a9f82e1ef4a670da86e2dd955aa863b3a14
yutong2000 <2741201152@qq.com>
2024-12-09 06:34:05 -0500

    Update LavariverBehavior.cs

--- catAfterlife/Assets/scripts/LavariverBehavior.cs
+++ catAfterlife/Assets/scripts/LavariverBehavior.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LavariverBehavior : MonoBehaviour
 {
     public GameObject Gem;
+    public GameObject colliderInMid;
+
+    // if the collider has already been diabled for 1 time,
+    // when the player entered for the second time shou
+    private bool diabledCollider = false;
+
+    public GameObject gemToPlace;
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +21,7 @@ public class LavariverBehavior : MonoBehaviour
 
         Debug.Log((gb.GetStatusOfGem()));
         // set Gem as active if player already handin potion in lava region
-        if (gb.GetStatusOfGem())
+        if (gb.GetStatusOfGem() && !diabledCollider)
         {
             Gem.SetActive(true);
         }
@@ -23,6 +30,10 @@ public class LavariverBehavior : MonoBehaviour
     // move gem on the river
     public void MoveGem()
     {
+        diabledCollider = true;
+        // disable the collider in the mid
+        colliderInMid.gameObject.SetActive(false);
 
+        // todo:
     }
 }
